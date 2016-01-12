183277fdd703569863c0c286a29179b3d6cba7a0
Yannick de Lange <yannick.l.88@gmail.com>
2016-01-12 21:57:49 +0100

    Cleaned up extension a bit

--- src/DependencyInjection/Compiler/FeaturesCompilerPass.php
+++ src/DependencyInjection/Compiler/FeaturesCompilerPass.php
@@ -21,11 +21,34 @@ class FeaturesCompilerPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        // find all resolvers
+        // configure the resolvers
+        $resolvers = $this->configureResolvers($container);
+
+        // configure the tags
+        $tags = $this->configureTags($container, $resolvers);
+
+        // replace all tagged features with correct feature tag
+        $this->replaceTaggedFeatures($container, $tags);
+    }
+
+    /**
+     * Configure the resolvers in the service container.
+     *
+     * @param ContainerBuilder $container
+     * @return string[]
+     */
+    private function configureResolvers(ContainerBuilder $container)
+    {
         $services  = $container->findTaggedServiceIds('feature.resolver');
         $resolvers = [];
 
         foreach ($services as $id => $options) {
+            if (!isset($options[0]['config-key'])) {
+                throw new InvalidArgumentException(sprintf(
+                    'The value for "config-key" is missing in the tag "features.tag" for service "%s".',
+                    $id
+                ));
+            }
             $config_key = $options[0]['config-key'];
 
             $resolvers[$config_key] = new Reference($id);
@@ -34,11 +57,31 @@ class FeaturesCompilerPass implements CompilerPassInterface
             ->getDefinition('features.factory')
             ->replaceArgument(0, $resolvers);
 
-        // configure the tags
+        return $resolvers;
+    }
+
+    /**
+     * Configure the tags in the service container.
+     *
+     * @param ContainerBuilder $container
+     * @param string[]         $configured_resolvers
+     * @return string[]
+     */
+    private function configureTags(ContainerBuilder $container, array $configured_resolvers)
+    {
         $tags = $container->getParameter('features.tags');
 
         foreach ($tags as $tag) {
             $options = $container->getParameter('features.tags.' . $tag . '.options');
+
+            // check if all the resolvers are actually configured
+            if (count($missing = array_diff(array_keys($options), array_keys($configured_resolvers))) > 0) {
+                throw new InvalidArgumentException(sprintf(
+                    'Unknown resolver(s) %s configured for feature tag "%s".',
+                    trim(json_encode(array_values($missing)), '[]'),
+                    $tag
+                ));
+            }
 
             $definition = new Definition(Feature::class);
             $definition->setFactory([new Reference('features.factory'), 'createFeature']);
@@ -47,10 +90,28 @@ class FeaturesCompilerPass implements CompilerPassInterface
             $container->setDefinition('features.tag.' . $tag, $definition);
         }
 
-        // replace all tagged features with correct feature tag
+        return $tags;
+    }
+
+    /**
+     * Replace default feature service arguments with the feature correct
+     * feature service for all tagged services.
+     *
+     * @param ContainerBuilder $container
+     * @param string[]         $tags
+     */
+    private function replaceTaggedFeatures(ContainerBuilder $container, array $tags)
+    {
         $services = $container->findTaggedServiceIds('feature.tag');
 
         foreach ($services as $id => $options) {
+            if (!isset($options[0]['tag'])) {
+                throw new InvalidArgumentException(sprintf(
+                    'The value for "tag" is missing in the tag "features.tag" for service "%s".',
+                    $id
+                ));
+            }
+
             $tag = $options[0]['tag'];
 
             if (!in_array($tag, $tags)) {
@@ -61,26 +122,15 @@ class FeaturesCompilerPass implements CompilerPassInterface
                 ));
             }
 
-            $this->replaceFeatureTags($container->getDefinition($id), 'features.tag.' . $tag);
-        }
-    }
+            $definition = $container->getDefinition($id);
 
-    /**
-     * Replace feature tag the arguments with the correct feature tag services.
-     *
-     * @param Definition $definition
-     * @param string     $tag
-     */
-    private function replaceFeatureTags(Definition $definition, $tag)
-    {
-        $arguments = $definition->getArguments();
+            foreach ($definition->getArguments() as $index => $argument) {
+                if (! $argument instanceof Reference || $argument->__toString() !== 'features.tag') {
+                    continue;
+                }
 
-        foreach ($arguments as $index => $argument) {
-            if (! $argument instanceof Reference || $argument->__toString() !== 'features.tag') {
-                continue;
+                $definition->replaceArgument($index, new Reference('features.tag.' . $tag));
             }
-
-            $definition->replaceArgument($index, new Reference($tag));
         }
     }
 }
